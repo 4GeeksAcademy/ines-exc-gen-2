a8c1291672b6e953ca7f1578dafb5eb32d22d427
inestell <166044521+inestell@users.noreply.github.com>
2024-05-18 00:44:22 +0000

    excuse gen done

--- src/app.js
+++ src/app.js
@@ -6,6 +6,42 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#button").addEventListener("click", () => {
+    document.getElementById("excuse").innerHTML = excuseGenerator();
+  });
 };
+
+function excuseGenerator() {
+  let who = [
+    "An elephant",
+    "A wise man",
+    "My teacher",
+    "The neighbours",
+    "A plane"
+  ];
+
+  let what = ["ate", "cried", "ran", "dived", "wrote"];
+
+  let where = [
+    "in the swimming pool",
+    "in the sofa",
+    "outside",
+    "at the driveway",
+    "at home"
+  ];
+
+  let when = [
+    "yesterday",
+    "last week",
+    "1000 thousand years ago",
+    "when I was born",
+    "this morning"
+  ];
+
+  let whoG = who[Math.floor(Math.random() * who.length)];
+  let whatG = what[Math.floor(Math.random() * what.length)];
+  let whereG = where[Math.floor(Math.random() * where.length)];
+  let whenG = when[Math.floor(Math.random() * when.length)];
+
+  return `${whoG} ${whatG} ${whereG} ${whenG}.`;
+}
